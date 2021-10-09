f85393e57557c7080c392f3b7839785e194e9f4d
Olivia Computer <email.olivia.computer@gmail.com>
2021-10-09 11:28:47 -0700

    Show toast if error on submit observation

--- app/store/draft.ts
+++ app/store/draft.ts
@@ -1,33 +1,32 @@
+import { Observation } from '../types';
 import { createSlice, SliceCaseReducers } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
 import { merge } from 'lodash';
 
-import { Observation } from '../types';
+const initialState = {
+  id: undefined,
+  name: '',
+  date: dayjs().format('YYYYMMDD'),
+  gpsHidden: false,
+  isCollectionLocation: true,
+  location: undefined,
+  latitude: undefined,
+  longitude: undefined,
+  altitude: undefined,
+  notes: undefined,
+  vote: undefined,
+  isUploaded: false,
+  hasChanges: false,
+};
 
-const slice = createSlice<Observation, SliceCaseReducers<Observation>, 'draft'>(
-  {
-    name: 'draft',
-    initialState: {
-      id: undefined,
-      name: '',
-      date: dayjs().format('YYYYMMDD'),
-      gpsHidden: false,
-      isCollectionLocation: true,
-      location: undefined,
-      latitude: undefined,
-      longitude: undefined,
-      altitude: undefined,
-      notes: undefined,
-      vote: undefined,
-      isUploaded: false,
-      hasChanges: false,
-    },
-    reducers: {
-      updateDraft: (state, action) => merge(state, action.payload),
-      clearDraft: () => undefined,
-    },
+const slice = createSlice({
+  name: 'draft',
+  initialState,
+  reducers: {
+    updateDraft: (state, action) => merge(state, action.payload),
+    clearDraft: () => initialState,
   },
-);
+});
 
 export const { updateDraft, clearDraft } = slice.actions;
 
